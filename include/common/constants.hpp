3b61e55f793fe1dfb5988740ae4a4d382edb8750
Patrick Niklaus <github@execfoo.de>
2026-01-01 23:42:05 +0100

    Add helper headers

---
+++ include/common/constants.hpp
@@ -0,0 +1,38 @@
+#ifndef GPUSSSP_CONSTANTS_H
+#define GPUSSSP_CONSTANTS_H
+
+#include <limits>
+#include <cstdint>
+#include <cmath>
+
+namespace gpusssp
+{
+namespace common
+{
+
+constexpr std::uint32_t INVALID_ID = std::numeric_limits<std::uint32_t>::max();
+// can be doubled without overflowing
+constexpr std::int32_t INF_WEIGHT = std::numeric_limits<std::int32_t>::max() / 2;
+
+constexpr double FIXED_POINT_RESOLUTION = 1000.0;
+//constexpr double FIXED_POINT_RESOLUTION = 10.0;
+
+constexpr double from_fixed(std::int32_t value)
+{
+    return value / common::FIXED_POINT_RESOLUTION;
+}
+
+constexpr std::int32_t to_fixed(double value)
+{
+    return value * common::FIXED_POINT_RESOLUTION;
+}
+
+inline std::int32_t to_upper_fixed(double value)
+{
+    return std::ceil(value * common::FIXED_POINT_RESOLUTION);
+}
+
+}
+}
+
+#endif
---
+++ include/common/irange.hpp
@@ -0,0 +1,19 @@
+#ifndef GPUSSSP_COMMON_INTEGER_RANGE_HPP
+#define GPUSSSP_COMMON_INTEGER_RANGE_HPP
+
+#include <ranges>
+
+namespace gpusssp
+{
+namespace common
+{
+
+template <std::integral I>
+auto irange(I begin, I end) {
+    return std::views::iota(begin, end);
+}
+
+}
+}
+
+#endif // INTEGER_RANGE_HPP
