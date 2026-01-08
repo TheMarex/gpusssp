90ae870b0aa3273968a296cb7ffde7b652111f35
Patrick Niklaus <github@execfoo.de>
2026-01-09 00:03:02 +0100

    Add utils for CSV handling

---
+++ include/common/string_util.hpp
@@ -0,0 +1,39 @@
+#ifndef GPUSSSP_COMMON_STRING_UTIL_HPP
+#define GPUSSSP_COMMON_STRING_UTIL_HPP
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+namespace gpusssp::common::detail {
+
+inline void split(std::vector<std::string> &tokens, const std::string &input, const std::string &delimiters) {
+    tokens.clear();
+    std::size_t start = 0;
+    while (true) {
+        std::size_t end = input.find_first_of(delimiters, start);
+        if (end == std::string::npos) {
+            tokens.push_back(input.substr(start));
+            break;
+        }
+        tokens.push_back(input.substr(start, end - start));
+        start = end + 1;
+    }
+}
+
+inline std::string join(const std::vector<std::string> &elements, const std::string &delimiter) {
+    if (elements.empty()) {
+        return "";
+    }
+    std::ostringstream os;
+    auto it = elements.begin();
+    os << *it++;
+    while (it != elements.end()) {
+        os << delimiter << *it++;
+    }
+    return os.str();
+}
+
+} // namespace gpusssp::common::detail
+
+#endif // GPUSSSP_COMMON_STRING_UTIL_HPP
---
+++ tests/test_string_util.cpp
@@ -0,0 +1,36 @@
+#include "common/string_util.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+#include <string>
+
+TEST_CASE("String split", "[string_util]") {
+    std::vector<std::string> tokens;
+    
+    SECTION("Split by single character") {
+        gpusssp::common::detail::split(tokens, "a,b,c", ",");
+        REQUIRE(tokens.size() == 3);
+        CHECK(tokens[0] == "a");
+        CHECK(tokens[1] == "b");
+        CHECK(tokens[2] == "c");
+    }
+
+    SECTION("Split with empty tokens") {
+        gpusssp::common::detail::split(tokens, "a,,c", ",");
+        REQUIRE(tokens.size() == 3);
+        CHECK(tokens[0] == "a");
+        CHECK(tokens[1] == "");
+        CHECK(tokens[2] == "c");
+    }
+}
+
+TEST_CASE("String join", "[string_util]") {
+    SECTION("Join multiple elements") {
+        std::vector<std::string> elements = {"a", "b", "c"};
+        CHECK(gpusssp::common::detail::join(elements, ",") == "a,b,c");
+    }
+
+    SECTION("Join empty vector") {
+        std::vector<std::string> elements;
+        CHECK(gpusssp::common::detail::join(elements, ",") == "");
+    }
+}
