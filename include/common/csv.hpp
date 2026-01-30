0e6922ab4088297a3aeba10d9c5b85ff6e899a01
Patrick Niklaus <github@execfoo.de>
2026-01-30 11:05:45 +0100

    Allow optional columns in CSV reader

--- include/common/csv.hpp
+++ include/common/csv.hpp
@@ -4,6 +4,7 @@
 #include "common/string_util.hpp"
 
 #include <fstream>
+#include <optional>
 #include <sstream>
 #include <string>
 #include <type_traits>
@@ -23,24 +24,89 @@ inline skip ignored;
 
 namespace detail
 {
+// Helper to detect if a type is std::optional
+template <typename T> struct is_optional : std::false_type
+{
+};
+
+template <typename T> struct is_optional<std::optional<T>> : std::true_type
+{
+};
+
+template <typename T> inline constexpr bool is_optional_v = is_optional<T>::value;
+
+// Helper to extract the inner type from std::optional
+template <typename T> struct optional_inner_type
+{
+    using type = T;
+};
+
+template <typename T> struct optional_inner_type<std::optional<T>>
+{
+    using type = T;
+};
+
+template <typename T> using optional_inner_type_t = typename optional_inner_type<T>::type;
+
+// Note: We don't do exhaustive compile-time validation of optional positions
+// as it causes excessive template instantiations. The runtime behavior will
+// work correctly: if a non-optional column follows an optional one and CSV
+// data is missing columns, it will throw a runtime error when trying to parse
+// the missing required column.
+
 template <std::size_t Column, class... Types>
 void parse_column(std::vector<std::string> &columns, std::tuple<Types...> &tuple)
 {
-    // clang-format off
-    if constexpr(std::is_same_v<double, typename std::tuple_element<Column, std::tuple<Types...>>::type>) {
-        std::get<Column>(tuple) = std::stof(columns[Column]);
-    } else if constexpr(std::is_same_v<int, typename std::tuple_element<Column, std::tuple<Types...>>::type>) {
-        std::get<Column>(tuple) = std::stoi(columns[Column]);
-    } else if constexpr(std::is_same_v<unsigned, typename std::tuple_element<Column, std::tuple<Types...>>::type>) {
-        std::get<Column>(tuple) = std::stoi(columns[Column]);
-    } else if constexpr(std::is_same_v<std::string, typename std::tuple_element<Column, std::tuple<Types...>>::type>) {
-        std::get<Column>(tuple).swap(columns[Column]);
-    } else if constexpr(std::is_same_v<csv::skip, typename std::tuple_element<Column, std::tuple<Types...>>::type>) {
-        std::get<Column>(tuple) = csv::ignored;
-    } else {
-        throw std::runtime_error("Can't parse type.");
+    using column_type = typename std::tuple_element<Column, std::tuple<Types...>>::type;
+    using inner_type = optional_inner_type_t<column_type>;
+
+    // Check if we have enough columns
+    if (Column >= columns.size())
+    {
+        // If this column is optional, set it to nullopt and continue
+        if constexpr (is_optional_v<column_type>)
+        {
+            std::get<Column>(tuple) = std::nullopt;
+        }
+        else
+        {
+            throw std::runtime_error("Missing required column at index " + std::to_string(Column));
+        }
     }
-    // clang-format on
+    else
+    {
+        // We have the column, parse it
+        // clang-format off
+        if constexpr (is_optional_v<column_type>) {
+            // Parse into the optional type
+            if constexpr(std::is_same_v<double, inner_type>) {
+                std::get<Column>(tuple) = std::stof(columns[Column]);
+            } else if constexpr(std::is_same_v<int, inner_type>) {
+                std::get<Column>(tuple) = std::stoi(columns[Column]);
+            } else if constexpr(std::is_same_v<unsigned, inner_type>) {
+                std::get<Column>(tuple) = std::stoi(columns[Column]);
+            } else if constexpr(std::is_same_v<std::string, inner_type>) {
+                std::get<Column>(tuple) = columns[Column];
+            } else if constexpr(std::is_same_v<csv::skip, inner_type>) {
+                std::get<Column>(tuple) = csv::ignored;
+            } else {
+                throw std::runtime_error("Can't parse type in optional.");
+            }
+        } else if constexpr(std::is_same_v<double, column_type>) {
+            std::get<Column>(tuple) = std::stof(columns[Column]);
+        } else if constexpr(std::is_same_v<int, column_type>) {
+            std::get<Column>(tuple) = std::stoi(columns[Column]);
+        } else if constexpr(std::is_same_v<unsigned, column_type>) {
+            std::get<Column>(tuple) = std::stoi(columns[Column]);
+        } else if constexpr(std::is_same_v<std::string, column_type>) {
+            std::get<Column>(tuple).swap(columns[Column]);
+        } else if constexpr(std::is_same_v<csv::skip, column_type>) {
+            std::get<Column>(tuple) = csv::ignored;
+        } else {
+            throw std::runtime_error("Can't parse type.");
+        }
+        // clang-format on
+    }
 
     // clang-format off
     if constexpr(Column + 1 < std::tuple_size<std::tuple<Types...>>::value) {
@@ -48,6 +114,30 @@ void parse_column(std::vector<std::string> &columns, std::tuple<Types...> &tuple
     }
     // clang-format on
 }
+
+// Count the number of required (non-optional) columns
+template <std::size_t Column, class... Types> struct count_required_columns_impl;
+
+// Base case: reached end of types
+template <std::size_t Column, class... Types>
+struct count_required_columns_impl<Column, std::tuple<Types...>>
+{
+    static constexpr std::size_t value = 0;
+};
+
+// Recursive case
+template <class T, class... Rest> struct count_required_columns_impl<0, std::tuple<T, Rest...>>
+{
+    static constexpr std::size_t current = is_optional_v<T> ? 0 : 1;
+    static constexpr std::size_t value =
+        current + count_required_columns_impl<0, std::tuple<Rest...>>::value;
+};
+
+template <class... Types> struct count_required_columns
+{
+    static constexpr std::size_t value =
+        count_required_columns_impl<0, std::tuple<Types...>>::value;
+};
 
 template <class... Types> std::tuple<Types...> parse_columns(std::vector<std::string> &columns)
 {
@@ -173,11 +263,26 @@ template <class... Types> class CSVReader
             if (tokens.size() == 0)
                 return true;
 
-            if (tokens.size() != std::tuple_size<output_t>::value)
+            constexpr std::size_t required_columns =
+                detail::count_required_columns<Types...>::value;
+            constexpr std::size_t total_columns = std::tuple_size<output_t>::value;
+
+            // Check if we have too few columns (less than required)
+            if (tokens.size() < required_columns)
             {
                 throw std::runtime_error("Could not parse line: \"" + line +
                                          "\". Number of columns is " +
-                                         std::to_string(tokens.size()));
+                                         std::to_string(tokens.size()) + " but at least " +
+                                         std::to_string(required_columns) + " are required.");
+            }
+
+            // Check if we have too many columns
+            if (tokens.size() > total_columns)
+            {
+                throw std::runtime_error("Could not parse line: \"" + line +
+                                         "\". Number of columns is " +
+                                         std::to_string(tokens.size()) + " but at most " +
+                                         std::to_string(total_columns) + " are expected.");
             }
 
             value = detail::parse_columns<Types...>(tokens);
