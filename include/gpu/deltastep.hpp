ef967782ed0238a846809c785e785a325c82bc8c
Patrick Niklaus <github@execfoo.de>
2026-03-02 01:09:56 +0100

    Refactor deltastep to record commands

--- include/gpu/deltastep.hpp
+++ include/gpu/deltastep.hpp
@@ -51,8 +51,8 @@ template <typename GraphT> class DeltaStep
               uint32_t relax_batch_size = DEFAULT_RELAX_BATCH_SIZE,
               uint32_t workgroup_size = DEFAULT_WORKGROUP_SIZE)
         : graph_buffers(graph_buffers), deltastep_buffers(deltastep_buffers),
-          statistics(statistics), device(device), delta(delta),
-          relax_batch_size(relax_batch_size), workgroup_size(workgroup_size)
+          statistics(statistics), device(device), delta(delta), relax_batch_size(relax_batch_size),
+          workgroup_size(workgroup_size)
     {
     }
 
@@ -104,31 +104,8 @@ template <typename GraphT> class DeltaStep
             {workgroup_size});
     }
 
-    template <typename QueueT>
-    uint32_t run(vk::CommandPool &cmd_pool,
-                 QueueT &queue,
-                 uint32_t src_node,
-                 uint32_t dst_node,
-                 DeltaStepTracer *tracer = nullptr)
+    void record_init_commands(vk::CommandBuffer &cmd_buf, uint32_t src_node)
     {
-        if (tracer)
-        {
-            tracer->start();
-        }
-
-        vk::CommandBuffer cmd_buf =
-            device.allocateCommandBuffers({cmd_pool, vk::CommandBufferLevel::ePrimary, 1})[0];
-
-        uint32_t num_nodes = graph_buffers.num_nodes();
-        const std::size_t max_buckets = common::INF_WEIGHT / delta;
-
-        uint32_t *gpu_min_changed_id = deltastep_buffers.min_changed_id();
-        uint32_t *gpu_max_changed_id = deltastep_buffers.max_changed_id();
-        uint32_t *gpu_best_distance = deltastep_buffers.best_distance();
-        uint32_t *gpu_max_distance = deltastep_buffers.max_distance();
-
-        auto dispatch_buffer = deltastep_buffers.dispatch_buffer();
-
         auto record_start =
             common::Statistics::start(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
         cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
@@ -142,37 +119,59 @@ template <typename GraphT> class DeltaStep
                                   vk::AccessFlagBits::eTransferRead},
             {},
             {});
-
         cmd_buf.end();
         common::Statistics::get().stop(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION,
                                        record_start);
-        queue.submit(vk::SubmitInfo{0, nullptr, nullptr, 1, &cmd_buf});
-        queue.waitIdle();
+    }
 
-        for (uint32_t bucket = 0; bucket < max_buckets; bucket++)
+    uint32_t record_relax_batch_commands(vk::CommandBuffer &cmd_buf,
+                                         uint32_t num_nodes,
+                                         uint32_t bucket,
+                                         uint32_t buffer_index)
+    {
+        auto record_start =
+            common::Statistics::start(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
+        cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+
+        PushConsts pc{0, 0, num_nodes, bucket, delta};
+
+        uint32_t current_buffer_idx = 1 - buffer_index;
+
+        auto dispatch_buffer = deltastep_buffers.dispatch_buffer();
+
+        for (uint32_t batch_iter = 0; batch_iter < relax_batch_size; ++batch_iter)
         {
-            common::Statistics::get().count(common::StatisticsEvent::DELTASTEP_BUCKET);
-            PushConsts pc{src_node, dst_node, num_nodes, bucket, delta};
+            cmd_buf.bindPipeline(vk::PipelineBindPoint::eCompute,
+                                 prepare_dispatch_pipeline.pipeline);
+            cmd_buf.bindDescriptorSets(
+                vk::PipelineBindPoint::eCompute,
+                prepare_dispatch_pipeline.layout,
+                0,
+                prepare_dispatch_pipeline.descriptor_sets[current_buffer_idx],
+                {});
+            cmd_buf.pushConstants(prepare_dispatch_pipeline.layout,
+                                  vk::ShaderStageFlagBits::eCompute,
+                                  0,
+                                  4,
+                                  &num_nodes);
+            cmd_buf.dispatch(1, 1, 1);
+            cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eComputeShader,
+                                    vk::PipelineStageFlagBits::eComputeShader,
+                                    vk::DependencyFlags{},
+                                    vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite,
+                                                      vk::AccessFlagBits::eShaderRead |
+                                                          vk::AccessFlagBits::eIndirectCommandRead},
+                                    {},
+                                    {});
 
-            uint32_t prev_buffer_idx = 0;
-            uint32_t curr_buffer_idx = 1;
-            auto prev_dispatch_desc_set = prepare_dispatch_pipeline.descriptor_sets[0];
-            auto current_dispatch_desc_set = prepare_dispatch_pipeline.descriptor_sets[1];
-            auto prev_desc_set = main_pipeline.descriptor_sets[0];
-            auto current_desc_set = main_pipeline.descriptor_sets[1];
+            cmd_buf.bindPipeline(vk::PipelineBindPoint::eCompute, main_pipeline.pipeline);
+            cmd_buf.bindDescriptorSets(vk::PipelineBindPoint::eCompute,
+                                       main_pipeline.layout,
+                                       0,
+                                       main_pipeline.descriptor_sets[current_buffer_idx],
+                                       {});
 
-            auto record_start = common::Statistics::start(
-                common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
-            cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
-            // For each bucket we need to do the first pass with all nodes.
-            // Essentially the first run identifies the nodes in the bucket,
-            // All subsequent runs are limited to that.
-            *gpu_min_changed_id = 0;
-            *gpu_max_changed_id = num_nodes - 1;
-            *gpu_best_distance = common::INF_WEIGHT;
-
-            deltastep_buffers.cmd_init_changed(cmd_buf, prev_buffer_idx);
-
+            deltastep_buffers.cmd_clear_changed(cmd_buf, current_buffer_idx);
             cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eTransfer,
                                     vk::PipelineStageFlagBits::eComputeShader,
                                     vk::DependencyFlags{},
@@ -182,101 +181,132 @@ template <typename GraphT> class DeltaStep
                                     {},
                                     {});
 
+            cmd_buf.pushConstants(
+                main_pipeline.layout, vk::ShaderStageFlagBits::eCompute, 0, sizeof(pc), &pc);
+            cmd_buf.dispatchIndirect(dispatch_buffer, 0);
+            cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eComputeShader,
+                                    vk::PipelineStageFlagBits::eComputeShader,
+                                    vk::DependencyFlags{},
+                                    vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite,
+                                                      vk::AccessFlagBits::eShaderRead},
+                                    {},
+                                    {});
+
+            current_buffer_idx = 1 - current_buffer_idx;
+        }
+
+        cmd_buf.end();
+        common::Statistics::get().stop(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION,
+                                       record_start);
+
+        return 1 - current_buffer_idx;
+    }
+
+    void
+    record_sync_commands(vk::CommandBuffer &cmd_buf, uint32_t dst_node, uint32_t changed_buffer_idx)
+    {
+        auto record_start =
+            common::Statistics::start(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
+        cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+
+        cmd_buf.pipelineBarrier(
+            vk::PipelineStageFlagBits::eComputeShader,
+            vk::PipelineStageFlagBits::eTransfer,
+            vk::DependencyFlags{},
+            vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite, vk::AccessFlagBits::eTransferRead},
+            {},
+            {});
+
+        deltastep_buffers.cmd_sync_results(cmd_buf, dst_node, changed_buffer_idx);
+
+        cmd_buf.pipelineBarrier(
+            vk::PipelineStageFlagBits::eTransfer,
+            vk::PipelineStageFlagBits::eHost,
+            vk::DependencyFlags{},
+            vk::MemoryBarrier{vk::AccessFlagBits::eTransferWrite, vk::AccessFlagBits::eHostRead},
+            {},
+            {});
+
+        cmd_buf.end();
+        common::Statistics::get().stop(common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION,
+                                       record_start);
+    }
+
+    template <typename QueueT>
+    uint32_t run(vk::CommandPool &cmd_pool,
+                 QueueT &queue,
+                 uint32_t src_node,
+                 uint32_t dst_node,
+                 DeltaStepTracer *tracer = nullptr)
+    {
+        if (tracer)
+        {
+            tracer->start();
+        }
+
+        uint32_t num_nodes = graph_buffers.num_nodes();
+        const std::size_t max_buckets = common::INF_WEIGHT / delta;
+
+        uint32_t *gpu_min_changed_id = deltastep_buffers.min_changed_id();
+        uint32_t *gpu_max_changed_id = deltastep_buffers.max_changed_id();
+        uint32_t *gpu_best_distance = deltastep_buffers.best_distance();
+        uint32_t *gpu_max_distance = deltastep_buffers.max_distance();
+
+        std::vector<vk::CommandBuffer> cmd_bufs =
+            device.allocateCommandBuffers({cmd_pool, vk::CommandBufferLevel::ePrimary, 3});
+        auto &init_cmd_buf = cmd_bufs[0];
+        auto &relax_cmd_buf = cmd_bufs[1];
+        auto &sync_cmd_buf = cmd_bufs[2];
+
+        record_init_commands(init_cmd_buf, src_node);
+        queue.submit(vk::SubmitInfo{0, nullptr, nullptr, 1, &init_cmd_buf});
+        queue.waitIdle();
+
+        for (uint32_t bucket = 0; bucket < max_buckets; bucket++)
+        {
+            common::Statistics::get().count(common::StatisticsEvent::DELTASTEP_BUCKET);
+
+            uint32_t buffer_idx = 0;
+
+            auto record_start = common::Statistics::start(
+                common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
+            init_cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
+
+            *gpu_min_changed_id = 0;
+            *gpu_max_changed_id = num_nodes - 1;
+            *gpu_best_distance = common::INF_WEIGHT;
+
+            deltastep_buffers.cmd_init_changed(init_cmd_buf, buffer_idx);
+
+            init_cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eTransfer,
+                                         vk::PipelineStageFlagBits::eComputeShader,
+                                         vk::DependencyFlags{},
+                                         vk::MemoryBarrier{vk::AccessFlagBits::eTransferWrite,
+                                                           vk::AccessFlagBits::eShaderRead |
+                                                               vk::AccessFlagBits::eShaderWrite},
+                                         {},
+                                         {});
+
+            init_cmd_buf.end();
+            common::Statistics::get().stop(
+                common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION, record_start);
+            queue.submit(vk::SubmitInfo{0, nullptr, nullptr, 1, &init_cmd_buf});
+            queue.waitIdle();
+
             bool converged = false;
             while (!converged)
             {
+                buffer_idx =
+                    record_relax_batch_commands(relax_cmd_buf, num_nodes, bucket, buffer_idx);
+                record_sync_commands(sync_cmd_buf, dst_node, buffer_idx);
 
-                for (uint32_t batch_iter = 0; batch_iter < relax_batch_size; ++batch_iter)
-                {
-
-                    cmd_buf.bindPipeline(vk::PipelineBindPoint::eCompute,
-                                         prepare_dispatch_pipeline.pipeline);
-                    cmd_buf.bindDescriptorSets(vk::PipelineBindPoint::eCompute,
-                                               prepare_dispatch_pipeline.layout,
-                                               0,
-                                               current_dispatch_desc_set,
-                                               {});
-                    cmd_buf.pushConstants(prepare_dispatch_pipeline.layout,
-                                          vk::ShaderStageFlagBits::eCompute,
-                                          0,
-                                          4,
-                                          &num_nodes);
-                    cmd_buf.dispatch(1, 1, 1);
-                    cmd_buf.pipelineBarrier(
-                        vk::PipelineStageFlagBits::eComputeShader,
-                        vk::PipelineStageFlagBits::eComputeShader,
-                        vk::DependencyFlags{},
-                        vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite,
-                                          vk::AccessFlagBits::eShaderRead |
-                                              vk::AccessFlagBits::eIndirectCommandRead},
-                        {},
-                        {});
-
-                    cmd_buf.bindPipeline(vk::PipelineBindPoint::eCompute, main_pipeline.pipeline);
-                    cmd_buf.bindDescriptorSets(vk::PipelineBindPoint::eCompute,
-                                               main_pipeline.layout,
-                                               0,
-                                               current_desc_set,
-                                               {});
-
-                    deltastep_buffers.cmd_clear_changed(cmd_buf, curr_buffer_idx);
-                    cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eTransfer,
-                                            vk::PipelineStageFlagBits::eComputeShader,
-                                            vk::DependencyFlags{},
-                                            vk::MemoryBarrier{vk::AccessFlagBits::eTransferWrite,
-                                                              vk::AccessFlagBits::eShaderRead |
-                                                                  vk::AccessFlagBits::eShaderWrite},
-                                            {},
-                                            {});
-
-                    cmd_buf.pushConstants(main_pipeline.layout,
-                                          vk::ShaderStageFlagBits::eCompute,
-                                          0,
-                                          sizeof(pc),
-                                          &pc);
-                    cmd_buf.dispatchIndirect(dispatch_buffer, 0);
-                    cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eComputeShader,
-                                            vk::PipelineStageFlagBits::eComputeShader,
-                                            vk::DependencyFlags{},
-                                            vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite,
-                                                              vk::AccessFlagBits::eShaderRead},
-                                            {},
-                                            {});
-
-                    std::swap(prev_buffer_idx, curr_buffer_idx);
-                    std::swap(prev_dispatch_desc_set, current_dispatch_desc_set);
-                    std::swap(prev_desc_set, current_desc_set);
-                }
-
-                cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eComputeShader,
-                                        vk::PipelineStageFlagBits::eTransfer,
-                                        vk::DependencyFlags{},
-                                        vk::MemoryBarrier{vk::AccessFlagBits::eShaderWrite,
-                                                          vk::AccessFlagBits::eTransferRead},
-                                        {},
-                                        {});
-
-                // After the swap, prev_buffer_idx has the latest results
-                deltastep_buffers.cmd_sync_results(cmd_buf, dst_node, prev_buffer_idx);
-
-                cmd_buf.pipelineBarrier(vk::PipelineStageFlagBits::eTransfer,
-                                        vk::PipelineStageFlagBits::eHost,
-                                        vk::DependencyFlags{},
-                                        vk::MemoryBarrier{vk::AccessFlagBits::eTransferWrite,
-                                                          vk::AccessFlagBits::eHostRead},
-                                        {},
-                                        {});
-
-                cmd_buf.end();
-                common::Statistics::get().stop(
-                    common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION, record_start);
-                queue.submit(vk::SubmitInfo{0, nullptr, nullptr, 1, &cmd_buf});
+                const std::array<vk::CommandBuffer, 2> batch_bufs = {relax_cmd_buf, sync_cmd_buf};
+                queue.submit(vk::SubmitInfo{0, nullptr, nullptr, 2, batch_bufs.data()});
                 queue.waitIdle();
 
                 if (tracer)
                 {
-                    tracer->signal_and_wait(
-                        {.bucket_index = bucket, .buffer_index = prev_buffer_idx});
+                    tracer->signal_and_wait({.bucket_index = bucket, .buffer_index = buffer_idx});
                 }
 
                 uint32_t range_size = std::max(0L,
@@ -289,12 +319,6 @@ template <typename GraphT> class DeltaStep
                     << " ~ " << static_cast<double>(range_size) / num_nodes << '\n';
 
                 converged = *gpu_min_changed_id >= num_nodes;
-                if (!converged)
-                {
-                    record_start = common::Statistics::start(
-                        common::StatisticsEvent::DELTASTEP_CMDBUF_RECORD_DURATION);
-                    cmd_buf.begin({vk::CommandBufferUsageFlagBits::eOneTimeSubmit});
-                }
             }
 
             if (*gpu_best_distance != common::INF_WEIGHT)
@@ -314,6 +338,8 @@ template <typename GraphT> class DeltaStep
                 break;
             }
         }
+
+        device.freeCommandBuffers(cmd_pool, cmd_bufs);
 
         if (tracer)
         {
